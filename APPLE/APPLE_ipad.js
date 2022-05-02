1ab3ef9969d3bf953e2b1f1088929bd7d4b3e772
Hana <railgun101014@gmail.com>
2022-05-03 02:17:43 +0800

    .

--- APPLE/APPLE_ipad.js
+++ APPLE/APPLE_ipad.js
@@ -4,6 +4,10 @@ let wifi = 0;
 let wifi_4G =0 ;
 const url = "https://raw.githubusercontent.com/apprunner/FileStorage/master/iPadAir2020_Data.json";
 
+window.onload = function () {
+
+}
+
 function change_img(color) {
     let ipad_img = document.getElementById('ipad_img');
     ipad_img.setAttribute('src',color+'.png')
@@ -26,7 +30,6 @@ function storage(_gb) {
     }
     p_price.innerText='NT$'+ price;
 }
-
 
 function connect(tools) {
     let p_price = document.getElementById('price');
@@ -54,24 +57,15 @@ function connect(tools) {
 }
 
 
-
-
-
-
         
 let a = '';
 let xhr = new XMLHttpRequest();
-
-        function requestJSON1(){
-
-            xhr.onload=function(){
-                msg.innerText = this.responseText;
-                console.log(this);
-                console.log(xhr);
-
-            }
-
-            xhr.open("GET",url);
-
-            xhr.send();
-        }
+function requestJSON1(){
+    xhr.onload=function(){
+        msg.innerText = this.responseText;
+        console.log(this);
+        console.log(xhr);
+    }
+    xhr.open("GET",url);
+    xhr.send();
+}
