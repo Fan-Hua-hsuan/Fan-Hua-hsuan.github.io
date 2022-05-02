bcda118f07ad691d6837cdd0659e23e4b6b93450
Hana <railgun101014@gmail.com>
2022-05-03 03:45:27 +0800

    .

--- APPLE/APPLE_ipad.js
+++ APPLE/APPLE_ipad.js
@@ -56,6 +56,28 @@ function connect(tools) {
     p_price.innerText='NT$'+ price;
 }
 
+// 迭代
+let color = ["gray","pink","Purple","blue","starlight"]
+color.forEach((value,index)=>{
+    let d = document.createElement('div');
+    let i = document.createElement('img');
+    let p = document.createElement('p');
+    i.setAttribute("src",color[index]+'.jpg');
+    p.innerText=color[index];
+    d.classList.add("col-6","border");
+    i.classList.add("img_style");
+    console.log(color[index]);
+    d.addEventListener("click",function(){
+        // 點擊改變圖片
+    change_img(color[index]);
+        // 點擊哪裡哪裡變色
+    });
+    d.append(i);
+    d.append(p);
+    let select_color_stlye = document.getElementById("select_color_stlye");
+    select_color_stlye.append(d);
+});
+// 儲存裝置區
 
         
 let a = '';
