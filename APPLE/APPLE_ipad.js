82bf98d16de8ec097986651e51f782a21e8aa1f7
Hana <railgun101014@gmail.com>
2022-05-03 04:20:27 +0800

    json

--- APPLE/APPLE_ipad.js
+++ APPLE/APPLE_ipad.js
@@ -2,10 +2,33 @@ let price =0;
 let gb =0;
 let wifi = 0;
 let wifi_4G =0 ;
-const url = "https://raw.githubusercontent.com/apprunner/FileStorage/master/iPadAir2020_Data.json";
+// const url = "https://raw.githubusercontent.com/apprunner/FileStorage/master/iPadAir2020_Data.json";
+//自己github上的資料
+const url = "https://fan-hua-hsuan.github.io/APPLE/ipadair.json";
+let ipad_json;
 
+//網頁載完之後 才載入json資料
 window.onload = function () {
-
+    let xhr = new XMLHttpRequest();    
+    xhr.onload = function () {
+        //將json網址 讀取之後轉成json格式
+        ipad_json = JSON.parse(this.responseText);
+        console.log(ipad_json);
+        //ex.抓第一筆id ipad[0].id
+        //ex. foreach 裡抓第一筆id ipad.id
+        ipad_json.forEach((value, index) => {
+            // console.log(value);
+            // msg.append(JSON.stringify(ipad), document.createElement("br"));
+            // _color = ["gray","png"]
+            _color = value.picture.split(".");
+            // _color = "gray"
+            _color = _color[0];
+            // console.log(index + "--" + value.id);
+        });
+    }
+    //打開網址
+    xhr.open("GET", url);
+    xhr.send();
 }
 
 function change_img(color) {
@@ -15,44 +38,30 @@ function change_img(color) {
 
 function storage(_gb) {
     let p_price = document.getElementById('price');
-    if(_gb==64){
-        wifi=17900;
-        wifi_4G=22900;
-        price=17900;
-        gb=_gb;
-    }
-    else
-    {
-        wifi=22900;
-        wifi_4G=27900;     
-        price=22900;   
-        gb=_gb;
-    }
+    //全部資訊都從JSON裡抓
+    ipad_json.forEach((value, index) => {
+        //判斷符合的 價錢是多少 預設是網路Wi-Fi的價錢
+        if(value.storage == _gb+"GB" && value.network=="Wi-Fi"){
+            price = value.price;
+            gb=_gb;
+        }
+        // 4G網路的價錢
+        else if(value.storage == _gb+"GB" && value.network=="Cellular"){
+            wifi_4G = value.price;
+        }
+    });
     p_price.innerText='NT$'+ price;
 }
 
 function connect(tools) {
     let p_price = document.getElementById('price');
-    if(gb==64){
-        if(tools=='Wi-Fi'){
-        price=17900;
+    //全部資訊都從JSON裡抓
+    ipad_json.forEach((value, index) => {
+        //判斷符合的 價錢是多少 預設是網路Wi-Fi的價錢
+        if(value.storage == gb+"GB" && value.network==tools){
+            price = value.price;
         }
-        else{
-            
-        price=22900;
-        }
-    }
-    else // 256
-    {
-        if(tools=='Wi-Fi'){
-        price=22900;
-        }
-        else{
-            
-        price=27900;
-        }
-
-    }
+    });
     p_price.innerText='NT$'+ price;
 }
 
@@ -78,16 +87,3 @@ color.forEach((value,index)=>{
     select_color_stlye.append(d);
 });
 // 儲存裝置區
-
-        
-let a = '';
-let xhr = new XMLHttpRequest();
-function requestJSON1(){
-    xhr.onload=function(){
-        msg.innerText = this.responseText;
-        console.log(this);
-        console.log(xhr);
-    }
-    xhr.open("GET",url);
-    xhr.send();
-}
